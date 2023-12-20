85e3ba196b84e11c0b3035ba5f622fd78a0c2e45
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2023-12-20 19:09:06 +0200

    module

---
+++ Javascript/js_modules/module2.js
@@ -0,0 +1,26 @@
+ // tabs
+ export function makeTabs(){
+ let tabButtons = document.querySelectorAll('.tabButtons button');
+
+ let tabs = document.querySelectorAll('.tabContent div');
+
+ function showTab(id){
+     for(tab of tabs){
+         tab.style.display = "none";
+     }
+     document.querySelector("div#"+id).style.display = "block";
+ }
+
+ for(button of tabButtons){
+     button.onclick = (e)=>{
+         let tab = e.target.dataset.tab;
+         showTab(tab);
+     }
+ }
+ tabButtons[0].click();
+ // showTab("tab1");
+
+
+
+ // Dialog сделать вывод в диалоговое окно
+}
